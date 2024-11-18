c6f520fab07c3a8b6f7c737f498ec6e1c64ebe6c
BuiHoangDieu <buidieu2k1k1@gmail.com>
2024-11-18 14:47:21 +0700

    commit

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,12 +5,6 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  // authDomain: "mern-blog-2122cd.firebaseapp.com",
-  // projectId: "mern-blog-2122cd",
-  // storageBucket: "mern-blog-2122cd.appspot.com",
-  // messagingSenderId: "643321880399",
-  // appId: "1:643321880399:web:a675c6ab26823a85736f3a",
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
